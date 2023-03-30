bc75d88cd65df032ce8d288e8dcf9298c67fac12
girish12upadhye <girishupadhye12@gmail.com>
2023-03-30 14:28:03 +0530

    First commit

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo
+{
+	public Demo(String str)
+	{
+		System.out.println(str);
+	}
+	public static void main(String args[])
+	{
+		Demo d = new Demo("Demo Constructor with String Parameter");
+	}
+
+}
---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test
+{
+	public Test(String str)
+	{
+		System.out.println(str);
+	}
+	public static void main(String args[])
+	{
+		Test t = new Test("This is Test Class Constructor");
+	}
+}
