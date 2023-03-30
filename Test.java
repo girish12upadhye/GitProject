b34f9d8791fd8f4e560f2640c11756588c42baf6
girish12upadhye <girishupadhye12@gmail.com>
2023-03-30 15:31:31 +0530

    Feature Commit

--- Test.java
+++ Test.java
@@ -8,6 +8,10 @@ public class Test
 	{
 		System.out.println("Master Branch");
 	}
+	public static void featureBranch()
+	{
+		System.out.println("Feature Branch");
+	}
 	public static void main(String args[])
 	{
 		Test t = new Test("This is Test Class Constructor");
